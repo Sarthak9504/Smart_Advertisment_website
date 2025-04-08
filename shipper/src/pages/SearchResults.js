22fd8bd17d3682344fd8ed4954f960b044323ba5
Sarthak9504 <sarthakpithe04@gmail.com>
2025-04-08 12:47:21 +0530

    API call to server added

--- shipper/src/pages/SearchResults.js
+++ shipper/src/pages/SearchResults.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
 import SearchAdBanner from "../Components/SearchAdBanner/SearchAdBanner";
 import SearchProductList from "../Components/SearchResults/SearchProductList";
@@ -17,11 +18,42 @@ function SearchResults() {
     const location = useLocation();
     const query = new URLSearchParams(location.search).get("q") || "all";
     const adData = searchAds[query] || { adImage: "/images/default-ad.jpg", adLink: "/" };
+    const [adImage, setAdImage] = useState(null);
+
+    useEffect(() => {
+        // Parse document.cookie to get searchHistory
+        const cookies = document.cookie.split("; ");
+        const searchCookie = cookies.find((row) => row.startsWith("searchHistory="));
+
+        if (searchCookie) {
+            try {
+                const cookieValue = decodeURIComponent(searchCookie.split("=")[1]);
+                const searchHistory = JSON.parse(cookieValue);
+                const recentQuery = searchHistory[searchHistory.length - 1]; // Most recent search term
+
+                if (recentQuery) {
+                    // Make API call to fetch ad image
+                    axios
+                        .get(`http://172.19.1.181:8080/ads/${encodeURIComponent(recentQuery)}`)
+                        .then((response) => {
+                            // Expecting something like { image: "http://..." }
+                            console.log("response", response);
+                            setAdImage(response.data.image);
+                        })
+                        .catch((err) => {
+                            console.error("Error fetching ad image:", err);
+                        });
+                }
+            } catch (err) {
+                console.error("Failed to parse searchHistory cookie:", err);
+            }
+        }
+    }, []);
 
     return (
         <div>
             <h2>Search Results for "{query}"</h2>
-            <SearchAdBanner adImage={adData.adImage} adLink={adData.adLink} />
+            <SearchAdBanner adImage={adImage} adLink={adData.adLink} />
             <SearchProductList products={sampleProducts} />
         </div>
     );
